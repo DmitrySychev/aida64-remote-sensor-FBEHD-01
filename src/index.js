d69764d6df38f6d4bf3369ca46ac1dfffd19954c
Dmitry Sychev <dmitry.sychev@me.com>
2022-02-13 12:49:21 -0500

    refactored css and sensors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-  const url = "http://192.168.1.24:3050/sse"
+  const url = "http://192.168.1.12:3050/sse"
 
   const evtSource = new EventSource(url);
 
@@ -20,20 +20,25 @@ document.addEventListener("DOMContentLoaded", function(e) {
   const gpuMemGauge = event.data.split("|")[13].slice(11, 14)
   const gpuClockGauge = event.data.split("|")[9].slice(9, 17)
   const pumpRpmGauge = event.data.split("|")[19].slice(11, 15)
+  const cpuWattageGauge = event.data.split("|")[17].slice(11, 14)
+  const gpuWattageGauge = event.data.split("|")[21].slice(3, 6)
+  const fansGauge = event.data.split("|")[23].slice(11, 15)
+  const gpuFanGauge = event.data.split("|")[25].slice(7, 9)
+  const fanSpeedAsInt = parseInt(fansGauge)
 
   $("#GaugeMeter_CPU_Clock").gaugeMeter({text: cpuClockGauge, style: "Arch"});
-  $("#GaugeMeter_1").gaugeMeter({percent:cpuTempGauge, showvalue: true, min: 50, style: "Arch", text: cpuTempGauge, text_lower: cpuClockGauge});
+  $("#GaugeMeter_CPU_TEMP").gaugeMeter({percent:cpuTempGauge, showvalue: true, min: 50, style: "Arch", text: cpuTempGauge});
   $("#GaugeMeter_VRM").gaugeMeter({text: moboVrmGauge, style: "Arch"});
 
-  $("#GaugeMeter_2").gaugeMeter({percent:pchTempGauge, showvalue: true, min: 50, style: "Arch", text: pchTempGauge});
-  $("#GaugeMeter_DIMM").gaugeMeter({text: dimmTempGauge, style: "Arch"});
+  $("#GaugeMeter_CPU_WATTAGE").gaugeMeter({percent:cpuWattageGauge, showvalue: true, min: 20, style: "Arch", text: cpuWattageGauge});
+  $("#GaugeMeter_GPU_WATTAGE").gaugeMeter({text: gpuWattageGauge, style: "Arch"});
 
   $("#GaugeMeter_GPU_Clock").gaugeMeter({text: gpuClockGauge, style: "Arch"});
-  $("#GaugeMeter_3").gaugeMeter({percent:gpuTempGauge, showvalue: true, min: 50, style: "Arch", text: gpuTempGauge});
+  $("#GaugeMeter_GPU_TEMP").gaugeMeter({percent:gpuTempGauge, showvalue: true, min: 50, style: "Arch", text: gpuTempGauge});
   $("#GaugeMeter_GPU_MEM").gaugeMeter({text: gpuMemGauge, style: "Arch"});
 
-  $("#GaugeMeter_4").gaugeMeter({percent:waterTempGauge, showvalue: true, min: 50, style: "Arch", text: waterTempGauge});
-  $("#GaugeMeter_PUMP").gaugeMeter({text: pumpRpmGauge, style: "Arch"});
+  $("#GaugeMeter_FANS").gaugeMeter({used: fanSpeedAsInt, showvalue: true, min: 0, style: "Arch", text: fansGauge});
+  $("#GaugeMeter_GPU_FAN").gaugeMeter({text: gpuFanGauge, style: "Arch"});
 
   }
 });
